1046da56148e9438a1e136ea436262b412cbe843
DongSeok Kim <kimdks12@gmail.com>
2021-03-13 15:41:49 +0900

    완성ver1.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
+const toTopEl = document.querySelector("#to-top");
 
 searchEl.addEventListener("click", () => {
   searchInputEl.focus();
@@ -26,14 +27,30 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+
+      // to-top버튼 보이기
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
       });
+
+      // to-top버튼 숨기기
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
+
+toTopEl.addEventListener("click", () => {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach((fadeEl, index) => {
@@ -64,6 +81,17 @@ new Swiper(".promotion .swiper-container", {
   navigation: {
     prevEl: ".promotion .swiper-prev",
     nextEl: ".promotion .swiper-next",
+  },
+});
+
+new Swiper(".awards .swiper-container", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
   },
 });
 
@@ -109,3 +137,6 @@ spyEls.forEach((spyEl) => {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear; // 2021
