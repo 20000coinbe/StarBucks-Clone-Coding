e27e58c1774a5e224bd5b34127ba2f3930850325
Neo <kimdks12@gmail.com>
2021-03-22 17:07:48 +0900

    로그인 페이지 작성 완료

--- js/main.js
+++ js/main.js
@@ -119,6 +119,3 @@ spyEls.forEach((spyEl) => {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear; // 2021
